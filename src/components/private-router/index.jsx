e221307833bd7ab6b9628df8c84f7ba80bf46732
Nghi Ngo <nghingothanhnghi@gmail.com>
2023-11-26 11:41:12 +0700

    update

--- src/components/private-router/index.jsx
+++ src/components/private-router/index.jsx
@@ -4,29 +4,29 @@ import { useSelector } from 'react-redux';
 
 import { history } from '../../helpers';
 
-export { PrivateRoute };
+// export { PrivateRoute };
 
-function PrivateRoute() {
-    const auth = useSelector(x => x.auth.value);
-
-    if (!auth) {
-        // not logged in so redirect to login page with the return url
-        return <Navigate to="/login" state={{ from: history.location }} />
-    }
-
-    // authorized so return outlet for child routes
-    return <Outlet />;
-}
-
-// export default function PrivateRoute() {
+// function PrivateRoute() {
 //     const auth = useSelector(x => x.auth.value);
-//     return (
-//         <>
-//         {!auth && <Navigate to="/login" state={{ from: history.location }} />}
-//         {auth }
-        
-        
-        
-//         </>
-//     )
+
+//     if (!auth) {
+//         // not logged in so redirect to login page with the return url
+//         return <Navigate to="/login" state={{ from: history.location }} />
+//     }
+
+//     // authorized so return outlet for child routes
+//     return <Outlet />;
 // }
+
+export function PrivateRoute() {
+    const auth = useSelector(x => x.auth.value);
+    return (
+        <>   
+        {auth ?(
+            <Outlet />
+        ):(
+            <Navigate to="/login" state={{ from: history.location }} />
+        )}
+        </>
+    )
+}
